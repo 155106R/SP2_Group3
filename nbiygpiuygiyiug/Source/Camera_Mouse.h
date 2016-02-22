fe9227dbafabcb155cf084a272341479a95753fd
155106R <155106R@mymail.nyp.edu.sg>
2016-02-22 18:08:52 +0800

    =======================Yue xian Daily commit added jumpping to camera_mouse changed toga cpp model orientation

--- nbiygpiuygiyiug/Source/Camera_Mouse.h
+++ nbiygpiuygiyiug/Source/Camera_Mouse.h
@@ -21,6 +21,11 @@ public:
 	int screenMidX;
 	int screenMidY;
 
+	float maxjump;
+	float gravityForce;
+	float jumpForce;
+	bool _jump;
+
 	Camera_Mouse();
 	~Camera_Mouse();
 	virtual void Init(const Vector3& pos, const Vector3& target, const Vector3& up);
@@ -29,6 +34,7 @@ public:
 	bool Camera_Mouse::checkCamera(int minX, int minZ, int maxX, int maxZ);
 	virtual void boundCheck(float minX, float minZ, float maxX, float maxZ);
 	virtual void boundCheck2(float minX, float minZ, float maxX, float maxZ);
+	void jump(float dt);
 };
 
 #endif
